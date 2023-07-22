0c9988993b0ab0f1a5c0115e0a28089ddc92e380
강명석 <k4858678@gmail.com>
2023-07-23 01:08:16 +0900

    코테 공부 기록 7/23

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -1,32 +1,22 @@
 package Programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ProgrammingBasic {
     public static void main(String[] args) {
 
-        solution("aacddefg");
+
+        solution("Naver");
     }
 
     public static String solution(String s) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
-                continue;
-
-            } else if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
-                continue;
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isLowerCase(s.charAt(i))){
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            }else{
+                sb.append(Character.toLowerCase(s.charAt(i)));
             }
-            sb.append(s.charAt(i));
-            System.out.println(sb);
-
         }
-        answer = sb.toString();
-        return answer;
+        return sb.toString();
     }
-
 }
