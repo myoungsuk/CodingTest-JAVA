e3494850f9e90b67735ec02fec5dae07dc5a94de
강명석 <k4858678@gmail.com>
2023-07-24 16:59:58 +0900

    코테 공부 기록 7/24

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -1,19 +1,26 @@
 package Programmers;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProgrammingBasic {
     public static void main(String[] args) {
-        int[] arr = {12, 4, 15, 46, 38, -2, 15};
-        int[][] intervals = {{1,3}, {0, 4}};
+        int[] arr = {0, 1, 2, 3, 4, 5};
+        int[] query = {4, 1, 2};
 
-        solution(arr, intervals);
+        solution(arr , query);
     }
 
-    public static int[] solution(int[] arr, int[][] intervals) {
-        int[] answer = {};
-
-        return answer;
+    public static int[] solution(int[] arr, int[] query) {
+       for(int i = 0; i < query.length; i++){
+           if(i % 2 == 0 || i == 0){
+             arr = Arrays.copyOfRange(arr, 0 , query[i] + 1);
+           }else{
+              arr = Arrays.copyOfRange(arr, query[i] +1, arr.length);
+           }
+       }
+       return arr;
     }
 }
