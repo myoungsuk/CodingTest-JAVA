cd485c1c34a4657cf2ba59a8764bb679bf0e489f
강명석 <k4858678@gmail.com>
2023-08-14 18:19:53 +0900

    코테 공부 기록 8/14

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -5,26 +5,18 @@ import java.util.*;
 public class ProgrammingBasic {
     public static void main(String[] args) {
 
-        int[] arr = {0, 1, 1, 1, 1};
-        solution(arr, 4);
+        int[] rank = {3, 7, 2, 5, 4, 6, 1};
+        boolean[] attendance = {false, true, true, true, true, false, false};
+        solution(rank , attendance);
     }
 
-    public static List<Integer> solution(int[] arr, int k) {
+    public int solution(String num_str) {
 
-        List<Integer> list = new ArrayList<>();
-        int count = 0;
-
-        for(int i = 0; i < arr.length && count < k; i++){
-            if(i == 0 || arr[i] != arr[i-1]) {
-                list.add(arr[i]);
-                count++;
-            }
+        int answer = 0;
+        for(int i = 0; i < num_str.length(); i++){
+            answer += Integer.parseInt(String.valueOf(num_str.charAt(i)));
         }
 
-        while(list.size() < k){
-            list.add(-1);
-        }
-
-        return list;
+        return answer;
     }
 }
