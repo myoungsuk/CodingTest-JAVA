1528d0a5c398661380b62d1402c58cf0cc968ae7
Kang Myoungsuk <k4858678@gmail.com>
2023-08-23 16:57:54 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+
+        for(int i = 0; i < my_string.length(); i++){
+            char ch = my_string.charAt(i);
+            if(Character.isDigit(ch)){
+                answer += ch - '0';
+            }
+        }
+
+        return answer;
+    }
+}
