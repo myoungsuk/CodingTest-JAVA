2c0ca5dc58e23746e404bd05ac2724a903a52927
강명석 <k4858678@gmail.com>
2023-09-21 09:30:24 +0900

    9/21 공부

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -14,6 +14,10 @@ public class ProgrammingBasic {
     public static String solution(String s) {
         StringBuilder answer = new StringBuilder();
 
+        // 처음 문자 처리
+        char firstChar = s.charAt(0);
+        answer.append(Character.isAlphabetic(firstChar) ? Character.toUpperCase(firstChar) : firstChar);
+
         for(int i = 1; i < s.length(); i++){
             //현재 문자가 공백인 경우
             if(s.charAt(i) == ' ') {
@@ -22,7 +26,13 @@ public class ProgrammingBasic {
             }
 
             //이전 문자가 공백인 경우
-
+            if(s.charAt(i-1) == ' ') {
+                answer.append(Character.toUpperCase(s.charAt(i)));
+            }else {
+                answer.append(Character.toLowerCase(s.charAt(i)));
+            }
         }
+
+        return answer.toString();
     }
 }
