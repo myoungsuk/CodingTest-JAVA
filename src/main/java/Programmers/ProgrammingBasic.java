d8ade72027ef3f81ce65a9fd6910945438eb3031
강명석 <k4858678@gmail.com>
2023-11-02 09:22:44 +0900

    11 02 공부

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -6,40 +6,26 @@ public class ProgrammingBasic {
 
     public static void main(String[] args) {
 
-        int n = 78;
+        int n = 5;
         System.out.println(solution(n));
 
     }
 
     public static int solution(int n) {
-        int answer = 0;
 
-        String binaryN = Integer.toBinaryString(n);
-        int count = 0;
-        for(int i = 0; i < binaryN.length(); i++) {
-            if(binaryN.charAt(i) == '1'){
-                count++;
-            }
+        if(n == 1 || n == 2){
+            return 1;
         }
 
-        while(true){
-            n++;
-            int count2 = 0;
-            String check = Integer.toBinaryString(n);
-            for(int i = 0; i < check.length(); i++){
-                if(count2 > count){
-                    break;
-                }
-                if(check.charAt(i) == '1'){
-                    count2++;
-                }
-            }
-            if(count == count2){
-                answer = n;
-                break;
-            }
+        int[] fibo = new int[n+1];
+        fibo[1] = 1;
+        fibo[2] = 1;
+        for(int i = 3; i <= n; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
         }
-        return answer;
+
+        return fibo[n];
+
     }
 
 }
