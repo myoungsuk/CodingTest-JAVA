3b7dd92cadd574616d42ae63a38a52c964101d81
강명석 <k4858678@gmail.com>
2023-12-13 10:24:05 +0900

    12 13 공부

--- src/main/java/Programmers/ProgrammingBasic.java
+++ src/main/java/Programmers/ProgrammingBasic.java
@@ -6,41 +6,31 @@ public class ProgrammingBasic {
 
     public static void main(String[] args) {
 
-        int N = 5000;
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
 
-        System.out.println(solution(N));
+        System.out.println((solution(participant, completion)));
 
     }
 
-    public static int solution(int n) {
-        int ans = n;
-        int count = 0;
+    public static String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
 
-        //한 번에 K칸을 앞으로 점프, 현재까지온거리 * 2 에 해당하는 위치로 순간이동
-        //순간이동은 건전지 사용량 x
-        //K칸을 점프하는건 K만큼의 건전지 사용량이 듬
-        //순간이동을 하는게 더 효율적
-        //거리가 N만큼 떨어져 있는 곳으로 이동
-        //점프를 하는건 최소화하기
-        //사용하려는 건전지 사용량의 최솟값 구하기
-
-        //2500 1250 675 점프(674) 337 점프(336) 168 84 42 21 점프(20)
-        //10 5 점프(4) 2 1 점프(0)
-
-        //홀수일 경우 1씩 증가함 1씩 증가할때마다 count++
-
-        while(ans != 0){
-
-            if(ans % 2 != 0) {
-                ans--;
-                count++;
-
-            }else {
-                ans /= 2;
-            }
-
+        for(String player : participant){
+            hashMap.put(player, hashMap.getOrDefault(player,0) + 1);
         }
 
-        return count;
+        for(String player : completion) {
+            hashMap.put(player, hashMap.get(player) - 1);
+        }
+
+        for(String key : hashMap.keySet()){
+            if(hashMap.get(key) != 0){
+                return key;
+            }
+        }
+
+        return null;
+
     }
 }
